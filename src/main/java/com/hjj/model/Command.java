5ae98d9613cdd0a1878307af39f6d2aaf2643533
Dream250 <2032311416@qq.com>
2017-07-22 19:06:45 +0800

    修改

---
+++ src/main/java/com/hjj/model/Command.java
@@ -0,0 +1,75 @@
+package com.hjj.model;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+public class Command {
+    private int id;
+    private int userId;
+    private  String content;
+    private int entityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public int getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(int entityType) {
+        this.entityType = entityType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    private int entityType;
+    private Date createdDate;
+    private int status;
+
+
+}
