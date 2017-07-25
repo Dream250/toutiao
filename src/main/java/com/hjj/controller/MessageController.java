21c72b27d3a38b9bb06b87c7a89d8cf977f75584
Dream250 <2032311416@qq.com>
2017-07-25 09:52:16 +0800

    评论中心

---
+++ src/main/java/com/hjj/controller/MessageController.java
@@ -0,0 +1,113 @@
+package com.hjj.controller;
+
+
+import com.hjj.model.HostHolder;
+import com.hjj.model.Message;
+import com.hjj.model.User;
+import com.hjj.model.ViewObject;
+import com.hjj.service.MessageService;
+import com.hjj.service.UserService;
+import com.hjj.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+@Controller
+public class MessageController {
+    private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
+
+    @Autowired
+    MessageService messageService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    HostHolder hostHolder;
+
+    @RequestMapping(path = {"/msg/list"}, method = {RequestMethod.GET})
+    public String conversationDetail(Model model){
+        try{
+            int localUserId=hostHolder.getUser().getId();
+            List<ViewObject> conversations=new ArrayList<ViewObject>();
+            List<Message> conversationList=messageService.getConversationList(localUserId,0,10);
+            for(Message msg:conversationList){
+                ViewObject vo =new ViewObject();
+                vo.set("conversation",msg);
+                int targetId = msg.getFromId() == localUserId ? msg.getToId() : msg.getFromId();
+                User user = userService.getUser(targetId);
+               // vo.set("user",user);
+                vo.set("headUrl", user.getHeadUrl());
+                vo.set("userName", user.getName());
+                vo.set("targetId", targetId);
+                vo.set("totalCount", msg.getId());
+                vo.set("unreadCount", messageService.getUnreadCount(localUserId, msg.getConversationId()));
+                conversations.add(vo);
+            }
+            model.addAttribute("conversations",conversations);
+        }catch (Exception e){
+            logger.error("获取站内信列表失败！"+e.getMessage());
+        }
+        return "letter";
+    }
+
+    @RequestMapping(path = {"/msg/detail"}, method = {RequestMethod.GET})
+    public String conversationDetail(Model model, @RequestParam("conversationId") String conversationId) {
+        try {
+            List<ViewObject> messages = new ArrayList<>();
+            List<Message> conversationList = messageService.getConversationDetail(conversationId, 0, 10);
+            for (Message msg : conversationList) {
+                ViewObject vo = new ViewObject();
+                vo.set("message", msg);
+                User user = userService.getUser(msg.getFromId());
+                if (user == null) {
+                    continue;
+                }
+                vo.set("headUrl", user.getHeadUrl());
+                vo.set("userName", user.getName());
+                messages.add(vo);
+            }
+            model.addAttribute("messages", messages);
+            return "letterDetail";
+        } catch (Exception e) {
+            logger.error("获取站内信列表失败" + e.getMessage());
+        }
+        return "letterDetail";
+    }
+
+
+    @RequestMapping(path={"/msg/addMessage"},method = {RequestMethod.POST})
+    @ResponseBody
+    public String addMessage(
+            @RequestParam("fromId") int fromId,
+            @RequestParam("toId") int toId,
+            @RequestParam("content") String content
+    ){
+        try {
+            Message msg = new Message();
+            msg.setContent(content);
+            msg.setFromId(fromId);
+            msg.setToId(toId);
+            msg.setCreatedDate(new Date());
+            msg.setConversationId(fromId < toId ? String.format("%d_%d", toId, fromId) : String.format("%d_%d", fromId, toId));
+            messageService.addMessage(msg);
+            return Util.getJSONString(1, "成功");
+        }catch (Exception e){
+            logger.info("失败！"+e.getMessage());
+            return Util.getJSONString(0,"失败");
+        }
+    }
+
+
+}
---
+++ src/main/java/com/hjj/service/MessageService.java
@@ -0,0 +1,35 @@
+package com.hjj.service;
+
+import com.hjj.dao.MessageDAO;
+import com.hjj.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+@Service
+public class MessageService {
+    @Autowired
+    MessageDAO messageDAO;
+
+    public int addMessage(Message message){
+        return messageDAO.addMessage(message);
+    }
+
+    public List<Message> getConversationList(int userId, int offset, int limit) {
+        return messageDAO.getConversationList(userId, offset, limit);
+    }
+
+    public List<Message> getConversationDetail(String conversationId, int offset, int limit) {
+        return messageDAO.getConversationDetail(conversationId, offset, limit);
+    }
+
+    public int getUnreadCount(int userId, String conversationId) {
+        return messageDAO.getConversationUnReadCount(userId, conversationId);
+    }
+
+
+}
